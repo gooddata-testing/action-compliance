801c726d081175e19b5b4a71bce076296f1bb389
Tomáš Kouba <tomas.kouba@gooddata.com>
2023-08-18 13:36:38 +0200

    Fill README.md Add unit tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,41 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import {hasOnlySpecialAuthors, hasSpecialTitle, matchesTicketRegex} from '../src/checks'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+// test('throws invalid number', async () => {
+//   const input = parseInt('foo', 10)
+//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+// })
+
+// test('wait 500 ms', async () => {
+//   const start = new Date()
+//   await wait(500)
+//   const end = new Date()
+//   var delta = Math.abs(end.getTime() - start.getTime())
+//   expect(delta).toBeGreaterThan(450)
+// })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+// test('test runs', () => {
+//   process.env['INPUT_MILLISECONDS'] = '500'
+//   const np = process.execPath
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   const options: cp.ExecFileSyncOptions = {
+//     env: process.env
+//   }
+//   console.log(cp.execFileSync(np, [ip], options).toString())
+// })
+
+test('Special title check works', async () => {
+  expect(hasSpecialTitle("^TRIVIAL\\b.*", "TRIVIAL: Typo fix")).toEqual(true)
+  expect(hasSpecialTitle("^TRIVIAL\\b.*", "Some normal title")).toEqual(false)
+})
+
+test('Ticket regex check works', async () => {
+  expect(matchesTicketRegex(".*\\b[A-Z]{2,}-[0-9]+\\b.*", "INFRA-1234: Blah blah", "Description of blah")).toEqual(true)
+  expect(matchesTicketRegex(".*\\b[A-Z]{2,}-[0-9]+\\b.*", "Blah blah", "Resolves GD-888")).toEqual(true)
+  expect(matchesTicketRegex(".*\\b[A-Z]{2,}-[0-9]+\\b.*", "No ticket in title", "No ticket in description")).toEqual(false)
 })
---
+++ src/checks.ts
@@ -0,0 +1,86 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function hasOnlySpecialAuthors(SPECIAL_AUTHORS: string, token: string): Promise<boolean> {
+    const octokit = github.getOctokit(token)
+    const { eventName, payload: {repository: repo, pull_request: pull_request} } = github.context
+    if (eventName != "pull_request") {
+      core.setFailed("Unsupported event: ${eventName}")
+      return true
+    }
+    if (repo == undefined) {
+      core.setFailed("repo is undefined in github even context")
+      return true
+    }
+    if (pull_request == undefined) {
+      core.setFailed("pull_request is undefined in github even context")
+      return true
+    }
+
+    const commits = await octokit.rest.pulls.listCommits({
+        owner: repo.owner.login,
+        repo: repo.name,
+        pull_number: pull_request.number
+      })
+
+      var allCommitsHaveSpecialAuthors:boolean = true
+      for(let commit of commits.data) {
+        if(commit.author==null) {
+          break
+        }
+        var commit_author = commit.author.login
+        core.debug("author: " + commit_author)
+
+        var foundSpecialAuthor:boolean = false
+        for(let author of SPECIAL_AUTHORS.split('\n'))  {
+          core.debug("special author: " + author)
+          var r = RegExp(author)
+          if(r.test(commit_author)) {
+            core.debug("Special author matches commit author")
+            foundSpecialAuthor = true
+            break
+          } else {
+            core.debug("Special author does not match commit author")
+          }
+        };
+        if(!foundSpecialAuthor) {
+          allCommitsHaveSpecialAuthors = false
+          break
+        }
+      };
+      if(allCommitsHaveSpecialAuthors) {
+        core.info("All commits have special author => PR is compliant")
+        return true
+      }
+      core.debug("Not all commits have special author => checking further")
+      return false
+}
+
+function hasSpecialTitle(SPECIAL_TITLE_REGEXES: string, prTitle: string): boolean {
+    var foundMatchingSpecialRegex = false
+    for(let regex of SPECIAL_TITLE_REGEXES.split('\n')) {
+      var r = RegExp(regex)
+      if(r.test(prTitle)) {
+        foundMatchingSpecialRegex = true
+        break
+      }
+    }
+    if(foundMatchingSpecialRegex) {
+      core.info("Title mentions special keywoard => PR is compliant")
+      return true
+    }
+    core.debug("The title does not mention any special keyword  => checking further")
+    return false
+}
+
+function matchesTicketRegex(REQUIRED_TICKET_REGEX: string, prTitle: string, prDescription: string): boolean {
+    let r = RegExp(REQUIRED_TICKET_REGEX.trim())
+    if(r.test(prTitle) || r.test(prDescription)) {
+      core.info("Title or description matches the required ticket regular expression => PR is compliant")
+      return true
+    }
+    core.debug("Neither title nor description match the required ticket regular expression")
+    return false
+}
+
+export {hasOnlySpecialAuthors, hasSpecialTitle, matchesTicketRegex}
